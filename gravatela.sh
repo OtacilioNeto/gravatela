aad7a07118dbebc018e475994f698ad2cc9c5ae0
Otacilio <otaciliodearaujo@gmail.com>
2019-03-15 16:42:14 -0300

    Adicionado um suporte melhor a deteção de tela no OS X no script de gravar a tela.

--- gravatela.sh
+++ gravatela.sh
@@ -29,12 +29,17 @@ elif [ $# = 0 ]; then
 			exit 1
 		fi
 	else #avfoundation
-		DEVICE=`ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | grep "Capture screen 1" | awk '{print $6 $7 $8 $9}'`
-		if [ "$DEVICE" != "[2]Capturescreen1" ]; then
-			SCREENDEVICEINDEX=1
+		DEVICE0=`ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | grep "Capture screen 0" | awk '{print $7 $8 $9}'`
+		DEVICE1=`ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | grep "Capture screen 1" | awk '{print $7 $8 $9}'`
+		if [ "$DEVICE1" == "Capturescreen1" ]; then
+			SCREENDEVICEINDEX=`ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | grep "Capture screen 1" | awk '{print $6}' | awk -F "[" '{print $2}' | awk -F "]" '{print $1}'`
+		elif [ "$DEVICE0" == "Capturescreen0" ]; then
+			SCREENDEVICEINDEX=`ffmpeg -f avfoundation -list_devices true -i "" 2>&1 | grep "Capture screen 0" | awk '{print $6}' | awk -F "[" '{print $2}' | awk -F "]" '{print $1}'`
 		else
-			SCREENDEVICEINDEX=0
+			echo "Erro ao detectar o dispositivo"
+			exit 1
 		fi
+		echo "Usando dispositivo $SCREENDEVICEINDEX"
 	fi
 	unlink $OUT/$USER.VideoAudio.mkv
 	touch $TMP/$USER.VideoAudio.mkv
@@ -42,22 +47,23 @@ elif [ $# = 0 ]; then
 	if [ -f $TMP/$USER.gravacamera.pid  ]; then
 		if [ $GRAB = "x11grab" ]; then
 			if [ $OFFSET != 0 ]; then
-                        	ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -f x11grab -i :0.0+$OFFSET,0 -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
+                        	ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -f x11grab -i :0.0+$OFFSET,0 -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
                 	else
-                        	ffmpeg -y -loglevel error -video_size `xdpyinfo | grep 'dimensions:'| awk '{print $2}'` -framerate 30 -vsync 1 -f x11grab -i :0.0+$OFFSET,0 -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
+                        	ffmpeg -y -loglevel error -video_size `xdpyinfo | grep 'dimensions:'| awk '{print $2}'` -framerate 30 -f x11grab -i :0.0+$OFFSET,0 -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
                 	fi
 		else
-			ffmpeg -y -loglevel error -f avfoundation -framerate 30 -vsync 1 -pix_fmt nv12 -i "$SCREENDEVICEINDEX:" -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
+			ffmpeg -y -loglevel error -f avfoundation -framerate 30 -pix_fmt nv12 -i "$SCREENDEVICEINDEX:" -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
 		fi
 	else
 		if [ $GRAB = "x11grab" ]; then
 			if [ $OFFSET != 0 ]; then
-				ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -f x11grab -i :0.0+$OFFSET,0 -f alsa -ar 44100 -ac 2 -async 44100  -i hw:0 -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
+				ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -f x11grab -i :0.0+$OFFSET,0 -f alsa -ar 44100 -ac 2 -async 1  -i hw:0 -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
 			else
-				ffmpeg -y -loglevel error -video_size `xdpyinfo | grep 'dimensions:'| awk '{print $2}'` -framerate 30 -vsync 1 -f x11grab -i :0.0+$OFFSET,0 -f alsa -ar 44100 -ac 2 -async 44100  -i hw:0 -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
+				ffmpeg -y -loglevel error -video_size `xdpyinfo | grep 'dimensions:'| awk '{print $2}'` -framerate 30 -f x11grab -i :0.0+$OFFSET,0 -f alsa -ar 44100 -ac 2 -async 1  -i hw:0 -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
 			fi
 		else
 			# Aqui Grava com a tela e audio
+			echo "Gravando..."
                         ffmpeg -y -loglevel error -f avfoundation -framerate 30 -vsync 1 -pix_fmt nv12  -i "$SCREENDEVICEINDEX:0" -async 44100  -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &	
 		fi
 	fi
@@ -67,14 +73,14 @@ elif [ $# = 1 ] && [ -f $TMP/$USER.gravacamera.pid ]; then
         rm -rf cat $TMP/$USER.gravacamera.pid
 elif [ $# = 1 ]; then
 	#Vamos gravar da camera
-	# So tem o código compatível com Linux e FreeBSD. Falta o do OS X
+	# So tem o código compatível com Linux e FreeBSD. Falta o do OSX
 	unlink $OUT/$USER.CameraAudio.mkv
         touch $TMP/$USER.CameraAudio.mkv
         ln -s $TMP/$USER.CameraAudio.mkv $OUT/$USER.CameraAudio.mkv
 	if [ -f $TMP/$USER.gravatela.pid  ]; then
 		ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mkv &
 	else
-		ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -f alsa -ar 44100 -ac 2 -async 44100  -i hw:0 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mkv &
+		ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -f alsa -ar 44100 -ac 2 -async 44100 -i hw:0 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mkv &
 	fi
 	echo $! > $TMP/$USER.gravacamera.pid
 fi
