91c647b5d8852b34c2365e0c023b9d4428ef7613
Otacilio <otaciliodearaujo@gmail.com>
2020-09-07 11:52:16 -0300

    Voltei o formato de arquivo para mp4.

--- gravatela.sh
+++ gravatela.sh
@@ -54,33 +54,33 @@ elif [ $# = 0 ]; then
 		fi
 		echo "Usando dispositivo $SCREENDEVICEINDEX"
 	fi
-	if [ -f $OUT/$USER.VideoAudio.mp4 ]; then
-		unlink $OUT/$USER.VideoAudio.mp4
+	if [ -f $OUT/$USER.VideoAudio.mkv ]; then
+		unlink $OUT/$USER.VideoAudio.mkv
 	fi
-	touch $TMP/$USER.VideoAudio.mp4
-	ln -s $TMP/$USER.VideoAudio.mp4 $OUT/$USER.VideoAudio.mp4
+	touch $TMP/$USER.VideoAudio.mkv
+	ln -s $TMP/$USER.VideoAudio.mkv $OUT/$USER.VideoAudio.mkv
 	if [ -f $TMP/$USER.gravacamera.pid  ]; then
 		if [ $GRAB = "x11grab" ]; then
 			if [ $OFFSET != 0 ]; then
             	# Aqui também precisa calcular as dimensões da tela a ser gravada
-                ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 $VAAPI_DEVICE -f x11grab -i $DISPLAY+$OFFSET,0 $ENCODER -crf 0 $TMP/$USER.VideoAudio.mp4 &
+                ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 $VAAPI_DEVICE -f x11grab -i $DISPLAY+$OFFSET,0 $ENCODER -crf 0 $TMP/$USER.VideoAudio.mkv &
             else
-                ffmpeg -y -loglevel error -video_size `xdpyinfo | grep 'dimensions:'| awk '{print $2}'` -framerate 30 $VAAPI_DEVICE -f x11grab -i $DISPLAY+$OFFSET,0 $ENCODER -crf 0 $TMP/$USER.VideoAudio.mp4 &
+                ffmpeg -y -loglevel error -video_size `xdpyinfo | grep 'dimensions:'| awk '{print $2}'` -framerate 30 $VAAPI_DEVICE -f x11grab -i $DISPLAY+$OFFSET,0 $ENCODER -crf 0 $TMP/$USER.VideoAudio.mkv &
             fi
 		else
-			ffmpeg -y -loglevel error -f avfoundation -framerate 30 -pix_fmt nv12 -i "$SCREENDEVICEINDEX:" -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mp4 &
+			ffmpeg -y -loglevel error -f avfoundation -framerate 30 -pix_fmt nv12 -i "$SCREENDEVICEINDEX:" -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &
 		fi
 	else
 		if [ $GRAB = "x11grab" ]; then
 			if [ $OFFSET != 0 ]; then
-				ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 $VAAPI_DEVICE -f x11grab -i $DISPLAY+$OFFSET,0 -f alsa -ar 44100 -ac 2 -async 1  -i hw:0 $ENCODER -crf 0 $TMP/$USER.VideoAudio.mp4 &
+				ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 $VAAPI_DEVICE -f x11grab -i $DISPLAY+$OFFSET,0 -f alsa -ar 44100 -ac 2 -async 1  -i hw:0 $ENCODER -crf 0 $TMP/$USER.VideoAudio.mkv &
 			else
-				ffmpeg -y -loglevel error -video_size `xdpyinfo | grep 'dimensions:'| awk '{print $2}'` -framerate 30 $VAAPI_DEVICE -f x11grab -i $DISPLAY+$OFFSET,0 -f alsa -ar 44100 -ac 2 -async 1  -i hw:0 $ENCODER $TMP/$USER.VideoAudio.mp4 &
+				ffmpeg -y -loglevel error -video_size `xdpyinfo | grep 'dimensions:'| awk '{print $2}'` -framerate 30 $VAAPI_DEVICE -f x11grab -i $DISPLAY+$OFFSET,0 -f alsa -ar 44100 -ac 2 -async 1  -i hw:0 $ENCODER $TMP/$USER.VideoAudio.mkv &
 			fi
 		else
 			# Aqui Grava com a tela e audio
 			# Usar -vsync 1 faz com que o ffmpeg trave quando grava na saída HDMI
-            ffmpeg -y -loglevel error -f avfoundation -framerate 30 -pix_fmt nv12  -i "$SCREENDEVICEINDEX:0" -async 88200  -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mp4 &	
+            ffmpeg -y -loglevel error -f avfoundation -framerate 30 -pix_fmt nv12  -i "$SCREENDEVICEINDEX:0" -async 88200  -c:v libx264 -crf 0 -preset ultrafast $TMP/$USER.VideoAudio.mkv &	
 		fi
 	fi
 	echo $! > $TMP/$USER.gravatela.pid
@@ -92,18 +92,18 @@ elif [ $# = 1 ] && [ -f $TMP/$USER.gravacamera.pid ]; then
 elif [ $# = 1 ]; then
 	#Vamos gravar da camera
 	# So tem o código compatível com Linux e FreeBSD. Falta o do OSX
-	if [ -f $OUT/$USER.CameraAudio.mp4 ]; then
-		unlink $OUT/$USER.CameraAudio.mp4
+	if [ -f $OUT/$USER.CameraAudio.mkv ]; then
+		unlink $OUT/$USER.CameraAudio.mkv
 	fi
-    touch $TMP/$USER.CameraAudio.mp4
-    ln -s $TMP/$USER.CameraAudio.mp4 $OUT/$USER.CameraAudio.mp4
+    touch $TMP/$USER.CameraAudio.mkv
+    ln -s $TMP/$USER.CameraAudio.mkv $OUT/$USER.CameraAudio.mkv
 	if [ $GRAB = "x11grab" ]; then
 		if [ -f $TMP/$USER.gravatela.pid  ]; then	
 			# Se já está gravando a tela, grave a câmera sem áudio porque o processo que está gravando a tela já está lendo do microfone
-			ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mp4 &
+			ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mkv &
 		else
 			# Não está gravando a tela, então leia do microfone
-			ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -f alsa -ar 44100 -ac 2 -async 44100 -i hw:0 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mp4 &
+			ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -f alsa -ar 44100 -ac 2 -async 44100 -i hw:0 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mkv &
 		fi
 	else
 		# Aqui eh para o caso de estar usando o OS X
@@ -115,12 +115,12 @@ elif [ $# = 1 ]; then
 				echo "Opção não implementada"
 				exit 1
             	# Se já está gravando a tela, grave a câmera sem áudio porque o processo que está gravando a tela já está lendo do microfone
-                ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mp4 &
+                ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f v4l2 -i $1 -c:v copy /home/ota/Desktop/$USER.CameraAudio.mkv &
             else
 				echo "Não está funcionando a gravação direto da câmera do MAC"
 				exit 1
                 # Não está gravando a tela, então leia da camera
-                ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f avfoundation -pix_fmt nv12 -i "$SCREENDEVICEINDEX:1" -async 88200  -c:v copy /Users/otacilio/Desktop/$USER.CameraAudio.mp4 &
+                ffmpeg -y -loglevel error -video_size 1920x1080 -framerate 30 -vsync 1 -vcodec h264 -f avfoundation -pix_fmt nv12 -i "$SCREENDEVICEINDEX:1" -async 88200  -c:v copy /Users/otacilio/Desktop/$USER.CameraAudio.mkv &
             fi
 		else
 			echo "Precisa adicionar o código para gravar da câmera do MAC"
